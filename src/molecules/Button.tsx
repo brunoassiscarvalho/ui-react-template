77ac1f30e56348bee813af85a35b04cdb4a8dd5b
Bruno Assis Carvalho <brunoacarvalho@live.com>
2023-03-19 01:30:53 -0300

    first commit

---
+++ src/molecules/Button.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface IButton {
+  onClick?: () => void;
+}
+
+const Button = (props: IButton): JSX.Element => {
+  return <button>tGGGGGe</button>;
+};
+
+export default Button;
