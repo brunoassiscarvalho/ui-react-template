e76b7a0e610482c1f734488d19c21f0df8f7ed6e
Bruno Assis Carvalho <brunoacarvalho@live.com>
2023-04-04 05:57:30 -0300

    material ui

--- src/molecules/Button.tsx
+++ src/molecules/Button.tsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React from 'react';
+import { Button as MUIButton } from '@mui/material';
 
 interface IButton {
   onClick?: () => void;
 }
 
 const Button = (props: IButton): JSX.Element => {
-  return <button>tGGGGGe</button>;
+  return <MUIButton>AAAAAA</MUIButton>;
 };
 
 export default Button;
