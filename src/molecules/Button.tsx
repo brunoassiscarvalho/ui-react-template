d4cba3fcfb467b1c452ac14208eaa0cf33841761
Bruno Assis Carvalho <brunoacarvalho@live.com>
2023-05-29 07:50:51 -0300

    button component with type prop

--- src/molecules/Button.tsx
+++ src/molecules/Button.tsx
@@ -4,10 +4,11 @@ import { Button as MUIButton } from '@mui/material';
 interface IButton {
   onClick?: () => void;
   label: string;
+  type?: 'submit' | 'button' 
 }
 
-const Button = ({ label, onClick }: IButton): JSX.Element => {
-  return <MUIButton onClick={onClick}>{label}</MUIButton>;
+const Button = ({ label, onClick, type }: IButton): JSX.Element => {
+  return <MUIButton type={type} onClick={onClick}>{label}</MUIButton>;
 };
 
 export default Button;
